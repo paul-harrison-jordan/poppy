ca8ce537b96ab3fe2bf0aeb67eaa051fe61f57f2
Paul Jordan <paul.jordan@klaviyo.com>
2025-04-28 23:26:30 -0400

    fixed auth ts type issue

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,29 +1,63 @@
-import NextAuth from "next-auth";
+import NextAuth, {
+  NextAuthOptions,
+  Session,
+  Account,
+} from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+/* -------- extend the built-in types -------- */
+declare module "next-auth" {
+  interface Session {
+    /** Gmail / Drive access token we copy from the JWT */
+    accessToken?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    /** Copied from OAuth account once after sign-in */
+    accessToken?: string;
+  }
+}
+
+/* -------- auth config -------- */
+export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          scope: 'openid email profile https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/documents',
+          scope:
+            "openid email profile " +
+            "https://www.googleapis.com/auth/drive " +
+            "https://www.googleapis.com/auth/documents",
         },
       },
     }),
   ],
-  pages: {
-    signIn: '/auth/signin',
-  },
+  pages: { signIn: "/auth/signin" },
+
   callbacks: {
-    async jwt({ token, account }: any) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
+    async jwt({
+      token,
+      account,
+    }: {
+      token: JWT;
+      account?: Account | null;
+    }): Promise<JWT> {
+      if (account) token.accessToken = account.access_token;
       return token;
     },
-    async session({ session, token }: any) {
+
+    async session({
+      session,
+      token,
+    }: {
+      session: Session;
+      token: JWT;
+    }): Promise<Session> {
       session.accessToken = token.accessToken;
       return session;
     },
@@ -31,5 +65,4 @@ export const authOptions = {
 };
 
 const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST }; 
+export { handler as GET, handler as POST };
