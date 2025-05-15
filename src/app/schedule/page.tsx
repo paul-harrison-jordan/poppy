b47fcc64fe29c3545cdf60f4bdd18eee8f7e7e8c
Paul Jordan <paul.jordan@klaviyo.com>
2025-05-14 22:56:16 -0400

    removed form

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -3,13 +3,11 @@
 import React from 'react';
 import AppShell from '@/components/AppShell';
 import Scheduler from '@/components/Scheduler';
-import SyncForm from '@/components/SyncForm';
 
 export default function SchedulePage() {
   return (
     <AppShell>
       <div className="flex-1 min-h-0 flex flex-col w-full max-w-5xl mx-auto">
-        <SyncForm />
         <Scheduler />
       </div>
     </AppShell>
