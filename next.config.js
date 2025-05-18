82240ee8d5ff2f1a128ca8778fa5b574df0d570a
Paul Jordan <paul.jordan@klaviyo.com>
2025-05-18 16:34:49 -0400

    linter

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        os: false,
+        path: false,
+        child_process: false,
+        http2: false,
+        zlib: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
